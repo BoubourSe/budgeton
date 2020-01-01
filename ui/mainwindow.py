6ebaf205aaae496cd02958afcdae060996a5cd22
BoubourSe <svsoft@free.fr>
2020-01-01 16:13:04 +0100

    add slot for open button and browse for get file

--- ui/mainwindow.py
+++ ui/mainwindow.py
@@ -1,5 +1,6 @@
-from PySide2.QtWidgets import QMainWindow
+from PySide2.QtWidgets import QMainWindow, QFileDialog
 from PySide2.QtCore import QFile, Slot, SIGNAL, SLOT, QObject
+from pathlib import Path
 
 from PySide2.QtWidgets import QShortcut
 
@@ -17,6 +18,7 @@ class MainWindow(QMainWindow):
         # slot button
         self.ui.buttonCompte.clicked.connect(self.on_click_button_compte)
         QObject.connect(self.ui.actionClose, SIGNAL("triggered()"), self, SLOT('on_click_menu_button_close()'))
+        QObject.connect(self.ui.actionOuvrir, SIGNAL("triggered()"), self, SLOT('on_click_menu_button_open()'))
 
         # widget
         self.comptepage = ComptePage()
@@ -32,3 +34,10 @@ class MainWindow(QMainWindow):
     @Slot()
     def on_click_menu_button_close(self):
         self.close()
+
+    @Slot()
+    def on_click_menu_button_open(self):
+        """Open database file"""
+        w = QFileDialog(self, directory=str(Path.home().absolute()), caption="Selectionnez la base de donné", filter="Base de donnée SQLite (*.sqlite)")
+        w.open()
+
